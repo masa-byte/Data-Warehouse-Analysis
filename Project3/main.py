cab1c187d8a2f31d381e147caa607e074b91523b
Masa Cirkovic <masacirkovic2001@gmail.com>
2024-06-19 17:39:01 +0200

    small directory search change

--- Project3/main.py
+++ Project3/main.py
@@ -1,23 +1,31 @@
 import pandas as pd
+import os
 
 if __name__ == "__main__":
-    data = pd.read_csv("data/heart_failure_clinical_records.csv")
+    current_directory = os.path.dirname(__file__)
+    data_directory = os.path.join(current_directory, "data")
 
-    columns = data.columns
-    for column in columns:
-        unique_values_with_percentage = data[column].value_counts(normalize=True)
-        unique_values_with_percentage = unique_values_with_percentage.to_dict()
-        unique_values_with_percentage = {
-            key: f"{value:.2%}" for key, value in unique_values_with_percentage.items()
-        }
-        print(f"Column: {column}")
-        print(f"Unique values:")
-        if len(unique_values_with_percentage.keys()) > 20:
-            print(len(unique_values_with_percentage.keys()))
-            print(
-                min(unique_values_with_percentage.keys()),
-                max(unique_values_with_percentage.keys()),
-            )
-        else:
-            for key, value in unique_values_with_percentage.items():
-                print(f"{key}: {value}")
+    for file in os.listdir(data_directory):
+        file_path = os.path.join(data_directory, file)
+
+        data = pd.read_csv(file_path)
+
+        columns = data.columns
+        for column in columns:
+            unique_values_with_percentage = data[column].value_counts(normalize=True)
+            unique_values_with_percentage = unique_values_with_percentage.to_dict()
+            unique_values_with_percentage = {
+                key: f"{value:.2%}"
+                for key, value in unique_values_with_percentage.items()
+            }
+            print(f"Column: {column}")
+            print(f"Unique values:")
+            if len(unique_values_with_percentage.keys()) > 20:
+                print(len(unique_values_with_percentage.keys()))
+                print(
+                    min(unique_values_with_percentage.keys()),
+                    max(unique_values_with_percentage.keys()),
+                )
+            else:
+                for key, value in unique_values_with_percentage.items():
+                    print(f"{key}: {value}")
